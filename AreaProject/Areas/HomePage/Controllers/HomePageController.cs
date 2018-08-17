0f0a61fb265af7a09ea229f870cbb156bf82d2a5
Szylin <patrykszylin93@gmail.com>
2018-08-17 15:19:17 +0100

    Initial project commit

---
+++ AreaProject/Areas/HomePage/Controllers/HomePageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AreaProject.Areas.HomePage.Controllers
+{
+
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+    }
+
+
+
+    [Area("HomePage")]
+    public class HomePageController : Controller
+    {
+        private readonly List<Person> _people = new List<Person>
+        {
+            new Person{ Name = "Patrick", Age = 24, City = "Stockton", Country = "England", PersonID = 1},
+            new Person{ Name = "P", Age = 24, City = "Stockton", Country = "England", PersonID = 2},
+            new Person{ Name = "A", Age = 24, City = "Stockton", Country = "England", PersonID = 3},
+            new Person{ Name = "T", Age = 24, City = "Stockton", Country = "England", PersonID = 4},
+            new Person{ Name = "R", Age = 24, City = "Stockton", Country = "England", PersonID = 5},
+            new Person{ Name = "Y", Age = 24, City = "Stockton", Country = "England", PersonID = 6},
+            new Person{ Name = "K", Age = 24, City = "Stockton", Country = "England", PersonID = 7}
+        };
+
+
+        public IActionResult Index()
+        {
+            return View(_people);
+        }
+    }
+}
